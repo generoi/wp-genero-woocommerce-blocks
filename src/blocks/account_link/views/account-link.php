4ad89b3bab8fa3da89f19c4b2972811b4a28d132
Oskar Schöldström <m@oxy.fi>
2022-02-17 18:44:42 -0300

    Reuse navigation-link styling in account and cart links

--- src/blocks/account_link/views/account-link.php
+++ src/blocks/account_link/views/account-link.php
@@ -1,5 +1,7 @@
-<div class="<?php echo $block->classes; ?>">
-    <a class="wp-block-genero-account-link__item" href="<?php echo wc_get_page_permalink('myaccount'); ?>">
-        <?php echo $block->attributes->label ?? ''; ?>
+<div class="<?php echo $block->classes; ?> wp-block-navigation-item">
+    <a class="wp-block-genero-account-link__item wp-block-navigation-item__content" href="<?php echo wc_get_page_permalink('myaccount'); ?>">
+        <span class="wp-block-navigation-item__label">
+            <?php echo $block->attributes->label ?? ''; ?>
+        </span>
     </a>
 </div>
--- src/blocks/cart_link/views/cart-link.php
+++ src/blocks/cart_link/views/cart-link.php
@@ -1,6 +1,8 @@
-<div class="<?php echo $block->classes; ?>">
-    <a class="wp-block-genero-cart-link__item" href="<?php echo wc_get_page_permalink('cart'); ?>">
-        <?php echo $block->attributes->label ?? ''; ?>
-        <span data-cart-link-counter></span>
+<div class="<?php echo $block->classes; ?> wp-block-navigation-item">
+    <a class="wp-block-genero-cart-link__item wp-block-navigation-item__content" href="<?php echo wc_get_page_permalink('cart'); ?>">
+        <span class="wp-block-navigation-item__label">
+            <?php echo $block->attributes->label ?? ''; ?>
+            <span data-cart-link-counter></span>
+        </span>
     </a>
 </div>
