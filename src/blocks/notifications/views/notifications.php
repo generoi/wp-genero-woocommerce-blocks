14f220196924457110276dd175202e7cf0f62cd0
Oskar Schöldström <m@oxy.fi>
2023-06-16 08:54:44 -0300

    Fix unnecessary loop

--- src/blocks/notifications/views/notifications.php
+++ src/blocks/notifications/views/notifications.php
@@ -1,5 +1,3 @@
 <div class="<?php echo $block->classes; ?>">
-    <?php while ( have_posts() ) : the_post() ?>
-        <?php woocommerce_output_all_notices(); ?>
-    <?php endwhile; ?>
+    <?php woocommerce_output_all_notices(); ?>
 </div>
